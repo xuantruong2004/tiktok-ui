2a1e785579662332a6819f0e0212ac7dc12a1c72
xuantruong2004 <truongxuantp3@gmail.com>
2022-08-11 12:40:11 +0700

    update Layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
 import Sidebar from './Sidebar';
 import Header from '~/components/Layout/components/Header';
+import styles from './DefaultLayout.module.scss';
+import classnames from 'classnames/bind';
 
+const cx = classnames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
