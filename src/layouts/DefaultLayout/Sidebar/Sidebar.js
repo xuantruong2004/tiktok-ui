f008f8b946d06b1cd5a1efdf898fe6f6d9fedd4f
xuantruong2004 <truongxuantp3@gmail.com>
2022-08-17 10:10:30 +0700

    update sidebar

--- src/layouts/DefaultLayout/Sidebar/Sidebar.js
+++ src/layouts/DefaultLayout/Sidebar/Sidebar.js
@@ -1,11 +1,40 @@
 import classNames from 'classnames/bind';
+import config from '~/config';
 import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeIconActive,
+    UserGroupIconActive,
+    LiveIconActive,
+} from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h1>Sidebar</h1>
+            <Menu>
+                <MenuItem
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeIconActive />}
+                    title="For you"
+                    to={config.routes.home}
+                />
+                <MenuItem
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupIconActive />}
+                    title="Following"
+                    to={config.routes.following}
+                />
+                <MenuItem
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveIconActive />}
+                    title="LIVE"
+                    to={config.routes.live}
+                />
+            </Menu>
         </aside>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,14 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Profile from '~/pages/Profile';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.profile, component: Profile },
+    { path: config.routes.live, component: Live },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
